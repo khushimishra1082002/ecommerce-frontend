ea80a98894ba74278c396b9d36b074767771a600
nrtdev <dev@newrise.in>
2026-01-18 09:39:49 +0530

    add changes in save for letter product

--- src/pages/SaveForLetterProduct.tsx
+++ src/pages/SaveForLetterProduct.tsx
@@ -38,34 +38,33 @@ const SaveForLetterProduct = () => {
   if (loading) return <p>Loading saved products...</p>;
   if (savedProducts.length === 0) return <p>No products saved for later.</p>;
 
-const handleMoveToCart = async (productId: string, quantity: number) => {
-  if (!userId) return;
+  const handleMoveToCart = async (productId: string, quantity: number) => {
+    if (!userId) return;
 
-  try {
-    // 1️⃣ Add to cart
-    await dispatch(
-      addToCart({
-        userId,
-        productId,
-        quantity,
-      })
-    ).unwrap();
+    try {
+      // 1️⃣ Add to cart
+      await dispatch(
+        addToCart({
+          userId,
+          productId,
+          quantity,
+        }),
+      ).unwrap();
 
-    // 2️⃣ Remove from Save For Later (BACKEND)
-    await removeSaveForLaterData(userId, productId);
+      // 2️⃣ Remove from Save For Later (BACKEND)
+      await removeSaveForLaterData(userId, productId);
 
-    // 3️⃣ Update UI (FRONTEND)
-    setSavedProducts((prev) =>
-      prev.filter((item) => item.productId._id !== productId)
-    );
+      // 3️⃣ Update UI (FRONTEND)
+      setSavedProducts((prev) =>
+        prev.filter((item) => item.productId._id !== productId),
+      );
 
-    // 4️⃣ Navigate
-    navigate("/mainCartPage");
-  } catch (error) {
-    console.error("Move to cart failed", error);
-  }
-};
-
+      // 4️⃣ Navigate
+      navigate("/mainCartPage");
+    } catch (error) {
+      console.error("Move to cart failed", error);
+    }
+  };
 
   return (
     <div className="m-6">
@@ -75,44 +74,49 @@ const handleMoveToCart = async (productId: string, quantity: number) => {
 
       <div className="space-y-4">
         {savedProducts.map((item) => {
+          if (!item.productId) return null;
+
           const product = item.productId;
           const image = product?.image?.[0];
-
           return (
-          <>
-            <div
-              key={item._id}
-              className="grid grid-cols-5 gap-12 p-6 "
-            >
-             <div className="border border-black/10">
-               <img
-                src={`${conf.BaseURL}${conf.GetImageUrl}/${image}`}
-                alt={product.name}
-                className=" w-36 h-36 rounded m-auto"
-              />
-              </div>
+            <>
+              <div key={item._id} className="grid grid-cols-5 gap-12 p-6 ">
+                <div className="border border-black/10">
+                  <img
+                    src={`${conf.BaseURL}${conf.GetImageUrl}/${image}`}
+                   alt={product?.name || "Product"}
+                    className=" w-36 h-36 rounded m-auto"
+                  />
+                </div>
 
-              <div className="col-span-3">
-                <h3 className="font-heading font-medium text-[14px]
-                       line-clamp-2 leading-5  text-gray-800">{product.name}</h3>
-                <p className="text-sm text-gray-500">Price: ₹{product.price}</p>
-                <p className="text-sm text-gray-500">
-                  Quantity: {item.quantity}
-                </p>
-              </div>
+                <div className="col-span-3">
+                  <h3
+                    className="font-heading font-medium text-[14px]
+                       line-clamp-2 leading-5  text-gray-800"
+                  >
+                    {product?.name || "Product not available"}
+                  </h3>
+                  <p className="text-sm text-gray-500">
+                    Price: ₹{product?.price}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    Quantity: {item?.quantity}
+                  </p>
+                </div>
 
-            <div>
-                <button 
-                onClick={() =>
-                  handleMoveToCart(item.productId._id, item.quantity)
-                }
-                className=" bg-red-500 text-white p-2 rounded font-heading text-sm "
-              >
-                Move to Cart
-              </button>
-            </div>
-            </div>
-            <div className="w-full h-[1px] bg-black/10"></div></>
+                <div>
+                  <button
+                    onClick={() =>
+                      handleMoveToCart(item.productId._id, item.quantity)
+                    }
+                    className=" bg-red-500 text-white p-2 rounded font-heading text-sm "
+                  >
+                    Move to Cart
+                  </button>
+                </div>
+              </div>
+              <div className="w-full h-[1px] bg-black/10"></div>
+            </>
           );
         })}
       </div>
